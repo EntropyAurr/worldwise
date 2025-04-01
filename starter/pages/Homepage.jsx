5bc2feb6823c4536e9189f6f0c130091079b5186
Au Cao Thuy Duong <auduong.301@gmail.com>
2025-04-01 13:58:39 +0700

    update

--- starter/pages/Homepage.jsx
+++ starter/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import styles from "./Homepage.module.css";
-function Homepage() {
+
+export default function Homepage() {
   return (
     <main className={styles.homepage}>
       <section>
@@ -8,11 +9,8 @@ function Homepage() {
           <br />
           WorldWise keeps track of your adventures.
         </h1>
-
         <h2>A world map that tracks your footsteps into every city you can think of. Never forget your wonderful experiences, and show your friends how you have wandered the world.</h2>
       </section>
     </main>
   );
 }
-
-export default Homepage;
